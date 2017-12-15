879d7387760ab2aad5444a6b3e01fb5a56279349
root <root@han6.k401.loc>
2017-12-15 11:17:13 +0900

    2017/12/15_backup

---
+++ app/assets/javascripts/sidebar.js
@@ -0,0 +1,12 @@
+$(function(){
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+});
+$(function(){
+    $('.testda').click(function () {
+      $(this).text("クリックされました");
+    });
+  });
+ 
