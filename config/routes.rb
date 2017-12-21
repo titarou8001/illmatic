70f25bdd9df91ab7fd32adf384d8ae25d8c310f1
takumioka <0325takumimy@gmail.com>
2017-12-21 11:51:29 +0900

    manabe_new

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'student/register'
+
+  get 'student/password'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'top/index'
+  post 'student/password_comp'
+  post 'student/login_check'
 end
